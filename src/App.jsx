311a8bd785d30ed24e95f3a6f50efc3d4309a476
Axmin <ahwn@pm.me>
2020-09-23 14:52:11 +0545

    [feat] complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import React, { useState } from 'react';
 import SearchInput from './components/search-input';
 import './App.css';
+import sentences from './queries';
 
-const sentences = [
-  'pros and cons of {}',
-  'what is {}',
-  'why does {} work',
-  '{} alternatives',
-  'how to use {}',
-  'when to use {}',
-  'why use {}',
-  'where to find {}',
-];
-
-function App() {
-  const [query, setQuery] = useState('');
+const App = () => {
+  const [query, setQuery] = useState(localStorage.getItem('query'));
 
   const onChange = ({ target }) => {
+    localStorage.setItem('query', target.value);
     setQuery(target.value);
+    const len = query.length + 6;
+    target.style.width = len ? len + 'ch' : 'inherit';
   };
   return (
-    <div className="App">
-      {sentences.map((i) => (
-        <SearchInput sentence={i} value={query} onChange={onChange} />
-      ))}
-    </div>
+    <main>
+      <div className="App">
+        {/* TODO make a nav for google search config  */}
+        <nav>Settings</nav>
+
+        {sentences.map((i, n) => (
+          <SearchInput
+            index={n}
+            sentence={i}
+            value={query}
+            onChange={onChange}
+          />
+        ))}
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
