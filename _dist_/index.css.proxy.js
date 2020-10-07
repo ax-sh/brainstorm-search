dd7e4cfd9f30d1df5fd716b0bf38c7f7be5d39f5
Axmin <>
2020-10-07 15:00:17 +0545

    Updates

--- _dist_/index.css.proxy.js
+++
@@ -1,9 +0,0 @@
-
-const code = "body {\r\n  margin: 0;\r\n  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',\r\n    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',\r\n    sans-serif;\r\n  -webkit-font-smoothing: antialiased;\r\n  -moz-osx-font-smoothing: grayscale;\r\n  background-color: #111;\r\n  color: #fff;\r\n}\r\n\r\ninput,\r\nspan,\r\nbutton {\r\n  font-size: 1.5em;\r\n  background-color: #060a0c;\r\n  color: #fff;\r\n}\r\n\r\ncode {\r\n  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',\r\n    monospace;\r\n}\r\n";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
--- web_modules/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-0ded058e.js';
-export { r as default } from './common/index-0ded058e.js';
-
-
-
-var useRef = react.useRef;
-var useState = react.useState;
-export { useRef, useState };
